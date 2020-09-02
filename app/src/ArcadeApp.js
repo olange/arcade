ddba3e19a94eb79043d420ad15524251e22af78d
Olivier Lange <olange@petit-atelier.ch>
2020-09-02 23:35:36 +0200

    Fetch some data from within the web app

--- app/src/ArcadeApp.js
+++ app/src/ArcadeApp.js
@@ -5,28 +5,68 @@ export class ArcadeApp extends LitElement {
   static get properties() {
     return {
       title: { type: String },
-      page: { type: String },
+      books: { type: Array },
     };
+  }
+
+  constructor() {
+    super();
+    this.title = "D-Arcade";
+    this.books = undefined;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    if( typeof this.books === "undefined") {
+      this.fetchBooks();
+    }
+  }
+
+  async fetchBooksTakeTwo() {
+    const headers = new Headers();
+    headers.append( 'Content-Type', 'application/json');
+    headers.append( 'Accept', 'application/json');
+    headers.append( 'Accept-Encoding', 'gzip, deflate');
+    // headers.append( 'Connection', 'keep-alive');
+    // headers.append( 'DNT', '1');
+    // headers.append( 'Origin', 'http://localhost:4000');
+    headers.append( 'authorization', 'b2xhbmdlQHBldGl0LWF0ZWxpZXIuY2g=');
+
+    const query = '{ "query": "query {\n  books {\n    title\n  }\n}" }';
+    const queryAsBlob = new Blob([ query ], { type : 'application/json' });
+
+    const params = {
+      method: 'POST',
+      headers: headers,
+      body: queryAsBlob,
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    const response = await fetch( 'http://localhost:4000', params);
+    const jsonResponse = await response.json();
+    this.books = jsonResponse;
+  }
+
+  async fetchBooks() {
+    const response = await fetch( 'https://api.openbrewerydb.org/breweries');
+    const jsonResponse = await response.json();
+    this.books = jsonResponse;
   }
 
   static get styles() {
     return css`
       :host {
         min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        font-size: calc(10px + 2vmin);
-        color: #1a2b42;
-        max-width: 960px;
-        margin: 0 auto;
+        display: flex; flex-direction: column;
+        align-items: center; justify-content: flex-start;
+        font-size: calc(10px + 2vmin); color: #1a2b42;
+        max-width: 960px; margin: 0 auto;
         text-align: center;
       }
 
-      main {
-        flex-grow: 1;
-      }
+      main { flex-grow: 1; }
 
       .logo > svg {
         margin-top: 36px;
@@ -34,12 +74,15 @@ export class ArcadeApp extends LitElement {
       }
 
       @keyframes app-logo-spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
+        from { transform: rotate(0deg); }
+        to { transform: rotate(360deg); }
+      }
+
+      pre {
+        font-size: calc(12px + 0.5vmin);
+        text-align: left;
+        background-color:white;
+        margin: 1em; padding: 1em;
       }
 
       .app-footer {
@@ -47,9 +90,7 @@ export class ArcadeApp extends LitElement {
         align-items: center;
       }
 
-      .app-footer a {
-        margin-left: 5px;
-      }
+      .app-footer a { margin-left: 5px; }
     `;
   }
 
@@ -57,21 +98,15 @@ export class ArcadeApp extends LitElement {
     return html`
       <main>
         <div class="logo">${openWcLogo}</div>
-        <h1>My app</h1>
+        <h1>${this.title}</h1>
 
-        <p>Edit <code>src/ArcadeApp.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/developing/#code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        ${this.books
+          ? html`<pre>${JSON.stringify( this.books, null, 2)}</pre>`
+          : html`<p>Loading…</p>`}
       </main>
 
       <p class="app-footer">
-        Made with love by <a target="_blank" rel="noopener noreferrer" href="https://gongfu.io">Gōng-fu I/O</a>
+        Made with love at <a target="_blank" rel="noopener noreferrer" href="https://gongfu.io">Gōng-fu I/O</a>
       </p>
     `;
   }
