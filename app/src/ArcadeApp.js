1d596a79cc0beeb25f86201c795132489a087c58
Rudi Farkas <rudi.farkas@gmail.com>
2020-09-25 01:06:15 +0200

    ArcadeApp.fetchBooks: modify to use the apollo client

--- app/src/ArcadeApp.js
+++ app/src/ArcadeApp.js
@@ -1,26 +1,22 @@
-import { LitElement, html, css } from 'lit-element';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-import { openWcLogo } from './open-wc-logo.js';
+import { LitElement, html, css } from "lit-element";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { openWcLogo } from "./open-wc-logo.js";
 
-console.info( "ARCADE› Instantiating Apollo Client…");
+console.info("ARCADE› Instantiating Apollo Client…");
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache()
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
 });
 
 const BOOKS_QUERY = gql`
   query GetBooks {
     books {
-        title
+      title
       rating
       author
     }
-  }`;
-
-console.info( "ARCADE› Looking up available books…");
-client
-  .query({ query: BOOKS_QUERY })
-  .then( result => console.log( result));
+  }
+`;
 
 export class ArcadeApp extends LitElement {
   static get properties() {
@@ -39,55 +35,46 @@ export class ArcadeApp extends LitElement {
   connectedCallback() {
     super.connectedCallback();
 
-    if( typeof this.books === "undefined") {
+    if (typeof this.books === "undefined") {
       this.fetchBooks();
     }
   }
 
-  async fetchBooksTakeTwo() {
-    const headers = new Headers();
-    headers.append( 'Content-Type', 'application/json');
-    headers.append( 'Accept', 'application/json');
-    headers.append( 'Accept-Encoding', 'gzip, deflate');
-    // headers.append( 'Connection', 'keep-alive');
-    // headers.append( 'DNT', '1');
-    // headers.append( 'Origin', 'http://localhost:4000');
-    headers.append( 'authorization', 'b2xhbmdlQHBldGl0LWF0ZWxpZXIuY2g=');
-
-    const query = '{ "query": "query {\n  books {\n    title\n  }\n}" }';
-    const queryAsBlob = new Blob([ query ], { type : 'application/json' });
-
-    const params = {
-      method: 'POST',
-      headers: headers,
-      body: queryAsBlob,
-      mode: 'cors',
-      cache: 'default'
-    };
-
-    const response = await fetch( 'http://localhost:4000', params);
+  async fetchBooks1() {
+    const response = await fetch("https://api.openbrewerydb.org/breweries");
     const jsonResponse = await response.json();
     this.books = jsonResponse;
   }
 
-  async fetchBooks() {
-    const response = await fetch( 'https://api.openbrewerydb.org/breweries');
-    const jsonResponse = await response.json();
-    this.books = jsonResponse;
+  fetchBooks() {
+    console.log("fetchBooks: Looking up available books…");
+    client
+      .query({ query: BOOKS_QUERY })
+      .then((result) => {
+        console.log("result:", result);
+        console.log("result.data:", result.data);
+        this.books = result.data;
+      });
   }
 
   static get styles() {
     return css`
       :host {
         min-height: 100vh;
-        display: flex; flex-direction: column;
-        align-items: center; justify-content: flex-start;
-        font-size: calc(10px + 2vmin); color: #1a2b42;
-        max-width: 960px; margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        font-size: calc(10px + 2vmin);
+        color: #1a2b42;
+        max-width: 960px;
+        margin: 0 auto;
         text-align: center;
       }
 
-      main { flex-grow: 1; }
+      main {
+        flex-grow: 1;
+      }
 
       .logo > svg {
         margin-top: 36px;
@@ -95,15 +82,20 @@ export class ArcadeApp extends LitElement {
       }
 
       @keyframes app-logo-spin {
-        from { transform: rotate(0deg); }
-        to { transform: rotate(360deg); }
+        from {
+          transform: rotate(0deg);
+        }
+        to {
+          transform: rotate(360deg);
+        }
       }
 
       pre {
         font-size: calc(12px + 0.5vmin);
         text-align: left;
-        background-color:white;
-        margin: 1em; padding: 1em;
+        background-color: white;
+        margin: 1em;
+        padding: 1em;
       }
 
       .app-footer {
@@ -111,7 +103,9 @@ export class ArcadeApp extends LitElement {
         align-items: center;
       }
 
-      .app-footer a { margin-left: 5px; }
+      .app-footer a {
+        margin-left: 5px;
+      }
     `;
   }
 
@@ -122,12 +116,15 @@ export class ArcadeApp extends LitElement {
         <h1>${this.title}</h1>
 
         ${this.books
-          ? html`<pre>${JSON.stringify( this.books, null, 2)}</pre>`
+          ? html`<pre>${JSON.stringify(this.books, null, 2)}</pre>`
           : html`<p>Loading…</p>`}
       </main>
 
       <p class="app-footer">
-        Made with love at <a target="_blank" rel="noopener noreferrer" href="https://gongfu.io">Gōng-fu I/O</a>
+        Made with love at
+        <a target="_blank" rel="noopener noreferrer" href="https://gongfu.io"
+          >Gōng-fu I/O</a
+        >
       </p>
     `;
   }
