e0c4d9f029f81c4eb3556846744c1187f1c2c531
Rudi Farkas <rudi.farkas@gmail.com>
2020-11-07 18:20:06 +0100

    move 4 icons from separate files into new src/assets/arcade-icons.js

--- app/src/button-svg.js
+++ app/src/button-svg.js
@@ -1,9 +1,11 @@
 import { customElement, property, LitElement, html, css } from 'lit-element';
 
-import { bookshelf } from './assets/bookshelf.js';
-import { mobilegame } from './assets/mobilegame.js';
-import { burgericon } from './assets/burgericon.js';
-import { hexaicon } from './assets/hexaicon.js';
+import {
+  bookshelf,
+  burgericon,
+  hexaicon,
+  mobilegame,
+} from './assets/arcade-icons.js';
 
 let defaultIcon = html`<svg viewBox="0 0 500 500">
   <circle cx="250" cy="250" r="180" fill-opacity="0" stroke-width="35" />
