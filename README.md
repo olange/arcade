ab79d72692fea4f0089e08f954bd1d950ce6a767
Rudi Farkas <rudi.farkas@gmail.com>
2020-12-18 15:56:22 +0100

    update app.js: add assets/7-hexagons.png, rotate it; add README and comments

---
+++ README.md
@@ -0,0 +1,6 @@
+# Explore PIXI v5
+
+## View
+
+    1. launch a server, e.g. python -m http.server 9000
+    2. or, in VSCode, install and launch extension LivePreview
--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 PIXI.utils.sayHello();
+// Create the application
+const app = new PIXI.Application({
+  width: 600,
+  height: 400,
+});
+
+// Add the view to the DOM
+document.body.appendChild(app.view);
+
+// add display objects, like a sprite:
+
+let hexa = PIXI.Sprite.from("assets/7-hexagons.png");
+app.stage.addChild(hexa);
+hexa.anchor.set(0.5);
+hexa.x = 0;
+hexa.y = 0;
+
+// start animating
+animate();
+function animate() {
+  requestAnimationFrame(animate);
+
+  // rotate the sprite
+  hexa.rotation += 0.02;
+
+  // render the container
+  app.renderer.render(app.stage);
+}
