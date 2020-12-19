c1bc975c3b922fd21ddc30bd4694841b0186d3bb
Rudi Farkas <rudi.farkas@gmail.com>
2020-12-19 21:53:53 +0100

    add appButtons using class TextButton; move class TextButton to buttons.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
+import { TextButton } from "./buttons.js";
+
 PIXI.utils.sayHello();
+
 // Create the application
 const app = new PIXI.Application({
   width: 600,
   height: 400,
 });
 
-// Add the view to the DOM
+// Create the buttons application
+const appButtons = new PIXI.Application({
+  width: 600,
+  height: 60,
+});
+
+// Add the views to the DOM
+document.body.appendChild(appButtons.view);
 document.body.appendChild(app.view);
+
+// ---------------------------------------
+// app
 
 // add display objects, like a sprite:
 
@@ -27,3 +40,26 @@ function animate() {
   // render the container
   app.renderer.render(app.stage);
 }
+
+// ---------------------------------------
+// appButtons
+
+function textButtonUp(text) {
+  console.log(text);
+}
+
+appButtons.stage.addChild(
+  new TextButton(60, 30, "one", (text) => console.log(text))
+);
+
+appButtons.stage.addChild(
+  new TextButton(120, 30, "two", (text) => console.log(text))
+);
+var textButton3 = new TextButton(180, 30, "three", textButtonUp);
+var textButton4 = new TextButton(240, 30, "four", (text) => console.log(text));
+
+appButtons.stage.addChild(textButton3);
+appButtons.stage.addChild(textButton4);
+appButtons.stage.addChild(
+  new TextButton(300, 30, "five", (text) => console.log(text))
+);
