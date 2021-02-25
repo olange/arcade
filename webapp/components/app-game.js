09ad3eb1129cd4c8640622d311cd021158b3aebd
Olivier Lange <olange@petit-atelier.ch>
2021-02-25 11:54:16 +0100

    Global source code re-styling following Prettier shared config (#10)

--- webapp/components/app-game.js
+++ webapp/components/app-game.js
@@ -4,25 +4,30 @@ import * as PIXI from 'pixi.js';
 
 @customElement('app-game')
 export class AppGame extends LitElement {
-
-  @internalProperty({type: Object})
+  @internalProperty({ type: Object })
   app;
 
-  @internalProperty({type: Object})
+  @internalProperty({ type: Object })
   static pixiAppOptions = {
     backgroundColor: 0xffffff,
     antialias: true,
     autoDensity: true, // !!!
     resolution: 2,
-  }
+  };
 
   @query('div#container')
   containerEl;
 
   static get styles() {
     return css`
-      :host { display: block; height: 100vh; }
-      div#container { width: 100%; height: 100%; }
+      :host {
+        display: block;
+        height: 100vh;
+      }
+      div#container {
+        width: 100%;
+        height: 100%;
+      }
     `;
   }
 
@@ -38,19 +43,21 @@ export class AppGame extends LitElement {
       width: clientWidth,
       height: clientHeight,
     };
-    console.log( `AppGame › new Pixi.Application( ${clientWidth}x${clientHeight}px)`);
+    console.log(
+      `AppGame › new Pixi.Application( ${clientWidth}x${clientHeight}px)`,
+    );
     this.app = new PIXI.Application(options);
     this.containerEl.appendChild(this.app.view);
 
     this.app.loader
       .add('circle', '/assets/apple-touch-icon.png')
-      .load(( loader, resources) => {
-        const circle = new PIXI.Sprite( resources.circle.texture);
+      .load((loader, resources) => {
+        const circle = new PIXI.Sprite(resources.circle.texture);
         circle.x = clientWidth / 2;
         circle.y = clientHeight / 2;
         circle.anchor.x = 0.5;
         circle.anchor.y = 0.5;
-        console.log( `AppGame › makeCircle()`, circle);
+        console.log(`AppGame › makeCircle()`, circle);
         this.app.stage.addChild(circle);
         this.app.ticker.add(() => {
           circle.rotation += 0.01;
--- webapp/components/app-start.js
+++ webapp/components/app-start.js
@@ -3,24 +3,27 @@ import { customElement, property } from 'lit-element';
 
 @customElement('app-start')
 export class AppStart extends LitElement {
-  @property({type: String}) href;
+  @property({ type: String }) href;
 
   static get styles() {
     return css`
       :host {
-          display: inline-block;
-          margin: 36px 0 18px; }
+        display: inline-block;
+        margin: 36px 0 18px;
+      }
       .link {
-        color: white; background-color: #1262b3;
-        padding: 18px 36px; text-decoration: none;
-        border-radius: 9px; }
+        color: white;
+        background-color: #1262b3;
+        padding: 18px 36px;
+        text-decoration: none;
+        border-radius: 9px;
+      }
     `;
   }
 
   render() {
-    return html`
-      <a class="link" href="${this.href}">
-        <slot></slot>
-      </a>`;
+    return html` <a class="link" href="${this.href}">
+      <slot></slot>
+    </a>`;
   }
 }
