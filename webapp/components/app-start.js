c31a7d2432b47bad931026d21c19b1959cfcf632
Olivier Lange <olange@petit-atelier.ch>
2021-03-25 00:51:31 +0100

    Allows to select from available games defined in database

--- webapp/components/app-start.js
+++ webapp/components/app-start.js
@@ -1,9 +1,11 @@
 import { LitElement, html, css } from 'lit-element';
-import { customElement, property } from 'lit-element';
+import { customElement, internalProperty, property } from 'lit-element';
+import firebase from 'firebase/app';
 
 @customElement('app-start')
 export class AppStart extends LitElement {
   @property({ type: String }) href;
+  @internalProperty({ type: Boolean }) loading;
 
   static get styles() {
     return css`
@@ -18,12 +20,41 @@ export class AppStart extends LitElement {
         text-decoration: none;
         border-radius: 9px;
       }
+      .unresolved {
+        background-color: #dedede;
+      }
     `;
   }
 
+  constructor() {
+    super();
+    this.loading = true;
+    this._data = undefined;
+  }
+
+  async firstUpdated() {
+    if( !this._data) {
+      this._data = {};
+      await this.fetchData();
+    }
+  }
+
+  async fetchData() {
+    this.loading = true;
+    const querySnapshot = await firebase.firestore().collection("games").get();
+    querySnapshot.forEach(( doc) => {
+      console.log( `firestore › games › doc( ${doc.id}) => { name: ${doc.data().name} }`);
+      this._data[ doc.id] = doc.data();
+    });
+    this.loading = false;
+  }
+
   render() {
-    return html` <a class="link" href="${this.href}">
-      <slot></slot>
-    </a>`;
+    if( this.loading) {
+      return html`<a class="link unresolved">Loading available games…</a>`
+    }
+
+    return Object.entries( this._data).map(([ gameId, gameObj]) =>
+      html`<a class="link" href="${this.href}#${gameId}">${gameObj.name}</a> `)
   }
 }
