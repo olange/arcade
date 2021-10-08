2005acf0c038972ac5fa4da0876da4aeadc56000
Rudi Farkas <rudi.farkas@gmail.com>
2021-10-08 23:42:28 +0200

    add packageOptions: { knownEntrypoints: [...] } as suggested in snowpack diagnostic messages

--- webapp/snowpack.config.js
+++ webapp/snowpack.config.js
@@ -10,7 +10,22 @@ module.exports = {
     "components/": { url: "/components" }
   },
   plugins: [ '@snowpack/plugin-babel' ],
-  packageOptions: { polyfillNode: true },
+  packageOptions: {  
+    polyfillNode: true,
+    knownEntrypoints: [
+        "@lit/reactive-element",
+        "@lit/reactive-element/decorators/base.js",
+        "@lit/reactive-element/decorators/custom-element.js",
+        "@lit/reactive-element/decorators/property.js",
+        "@lit/reactive-element/decorators/state.js",
+        "@lit/reactive-element/decorators/event-options.js",
+        "@lit/reactive-element/decorators/query.js",
+        "@lit/reactive-element/decorators/query-all.js",
+        "@lit/reactive-element/decorators/query-async.js",
+        "@lit/reactive-element/decorators/query-assigned-nodes.js",
+    ]
+
+  },
   devOptions: { output: "stream" }, // add `secure: true` for HTTPS
   buildOptions: { out: "build/", metaUrlPath: "modules/", sourcemap: true },
   optimize: { minify: true, treeshake: true }
